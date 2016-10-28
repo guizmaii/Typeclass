9ddce585330646725cfd5bcc1d4d83d3af6e269f
Jules Ivanic <jules.ivanic@gmail.com>
2016-10-28 15:34:20 +0200

    Esthetic

--- exercise/src/main/scala/typeclass/std/Option.scala
+++ exercise/src/main/scala/typeclass/std/Option.scala
@@ -7,8 +7,8 @@ object option {
   implicit def optionSemigroup[A](implicit ev: Semigroup[A]): Semigroup[Option[A]] = new Semigroup[Option[A]] {
     def combine(x: Option[A], y: Option[A]): Option[A] = (x, y) match {
       case (None, None)       => None
-      case (Some(a), None)    => x
-      case (None, Some(a))    => y
+      case (Some(_), None)    => x
+      case (None, Some(_))    => y
       case (Some(a), Some(b)) => Some(ev.combine(a, b))
     }
   }
